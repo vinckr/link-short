56b474b286bbd4b4e0dc3b9fb5706a93bddae455
vinckr <vincent@ory.sh>
2022-03-29 22:29:10 +0200

    feat: frontend

---
+++ frontend/nuxt.config.js
@@ -0,0 +1,47 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'frontend',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/tailwindcss
+    '@nuxtjs/tailwindcss',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+  ],
+  publicRuntimeConfig: {
+    kratosUI: process.env.KRATOS_URL || 'http://127.0.0.1:4455',
+    kratosAPIURL: process.env.KRATOS_API_URL || 'http://127.0.0.1:4433',
+    apiURL: process.env.API_URL || 'http://127.0.0.1:8080'
+  },
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
